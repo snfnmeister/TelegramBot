c14a2b6745934be1e953db68de3d596fc5a28ef3
Igor Izhenbin <izhenbin_ia@lemz-t.com>
2020-09-09 11:07:51 +0700

    Added DB

--- DataBase.py
+++ DataBase.py
@@ -1,3 +1,21 @@
-import sqlite3
+import sqlite3  # import library for DB
 
-conn = sqlite3.connect("mydatabase.db")
+conn = sqlite3.connect('mydatabase.db')  # make connect to DB
+
+cursor = conn.cursor()  # make cursor for ask and get answer from DB
+
+cursor.execute("""INSERT INTO albums
+                  VALUES """)
+
+#cursor.execute("""CREATE TABLE Albums
+     #          (title text, artist text, release_date text, publisher text, media_type text)
+           #    """)
+
+#cursor.execute("SELECT Name FROM Artist ORDER BY Name LIMIT 3")
+#result = cursor.fetchall()
+#result1 = cursor.fetchall()
+
+#print(result)
+#print(result1)
+
+conn.close()
