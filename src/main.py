5793dcb2c4bab7575a83c965b3136d12c29938e8
Igor Izhenbin <izhenbin_ia@lemz-t.com>
2020-10-02 17:23:21 +0700

    added branch & fixed a bit

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import telebot  # telegram-bot API
-#open file with key
-key_value = '1021237352:AAFgrsGq5Q6SA-B7XnWWgLAOyLW1fif9t9k'
+
+# open file with key
+key_value = 'will be added'
 bot = telebot.TeleBot(key_value)
 
-#keyboard config
+# keyboard config
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('Favorites', 'Add to favorites')
-#keyboard1.row('Favorites', 'Add to favorites', 'Delete from favorites', 'Settings', 'Help')
+keyboard1.row('Favorites', 'Add to favorites', 'Help')
+# keyboard1.row('Favorites', 'Add to favorites', 'Delete from favorites', 'Settings', 'Help')
+
+favorites_list = ['URL1', 'URL2']
 
 
 @bot.message_handler(commands=['start'])
@@ -16,12 +19,16 @@ def start_message(message):
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == "Favorites":
-        bot.send_message(message.chat.id, 'Hello general Kenobi')
-    elif message.text.lower() == 'Add to favorites':
+    if message.text == "Favorites":
+        bot.send_message(message.chat.id, favorites_list[0] + '\n' + favorites_list[1])
+    elif message.text == 'Add to favorites':
         bot.send_message(message.chat.id, 'Bye general Kenobi')
+    elif message.text == 'Help':
+        bot.send_message(message.chat.id, 'Help')
     else:
         bot.send_message(message.chat.id, "I don't get it!")
 
 
 bot.polling()
+
+# elif message.text.lower() == 'Add to favorites':
