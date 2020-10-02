544ef94b82042450ef0999113f1db12f3264587e
Igor Izhenbin <izhenbin_ia@lemz-t.com>
2020-10-02 12:33:29 +0700

    added functions

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import telebot  # telegram-bot API
+
+bot = telebot.TeleBot('token will be added')
+
+#keyboard config
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard1.row('Help', 'Add')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Hello there!', reply_markup=keyboard1)
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'Hello':
+        bot.send_mesasge(message.chat.id, 'Hello general Kenobi')
+    elif message.text.lower() == 'Bye-bye':
+        bot.send_message(message.chat.id, 'Bye general Kenobi')
+
+
+bot.polling()
