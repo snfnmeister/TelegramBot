7fe2ef7fae2e3ea19da547b50b287490bf70b6eb
Igor Izhenbin <izhenbin_ia@lemz-t.com>
2020-10-02 17:09:19 +0700

    added branch & fixed a bit

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import telebot  # telegram-bot API
-
-bot = telebot.TeleBot('token will be added')
+#open file with key
+key_value = '1021237352:AAFgrsGq5Q6SA-B7XnWWgLAOyLW1fif9t9k'
+bot = telebot.TeleBot(key_value)
 
 #keyboard config
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('Help', 'Add')
+keyboard1.row('Favorites', 'Add to favorites')
+#keyboard1.row('Favorites', 'Add to favorites', 'Delete from favorites', 'Settings', 'Help')
 
 
 @bot.message_handler(commands=['start'])
@@ -14,9 +16,9 @@ def start_message(message):
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == 'Hello':
-        bot.send_mesasge(message.chat.id, 'Hello general Kenobi')
-    elif message.text.lower() == 'Bye-bye':
+    if message.text.lower() == "Favorites":
+        bot.send_message(message.chat.id, 'Hello general Kenobi')
+    elif message.text.lower() == 'Add to favorites':
         bot.send_message(message.chat.id, 'Bye general Kenobi')
     else:
         bot.send_message(message.chat.id, "I don't get it!")
