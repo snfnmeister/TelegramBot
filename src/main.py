6d02da4b5484e2fa6b34f0f3f32fb7c8dd5b5498
Igor Izhenbin <izhenbin_ia@lemz-t.com>
2020-10-02 12:36:54 +0700

    fixed a bit

--- src/main.py
+++ src/main.py
@@ -18,6 +18,8 @@ def send_text(message):
         bot.send_mesasge(message.chat.id, 'Hello general Kenobi')
     elif message.text.lower() == 'Bye-bye':
         bot.send_message(message.chat.id, 'Bye general Kenobi')
+    else:
+        bot.send_message(message.chat.id, "I don't get it!")
 
 
 bot.polling()
