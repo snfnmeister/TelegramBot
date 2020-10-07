4828258c600c5d65f1a5cd0f786922cb5d2dc62c
Igor Izhenbin <izhenbin_ia@lemz-t.com>
2020-10-07 22:12:44 +0700

    added list append and favorites unit

---
+++ src/favorites_unit.py
@@ -0,0 +1,8 @@
+class FavoritesUnit:
+
+    def __init__(self):
+        self.uri = None
+        self.name = 'name'
+        self.new_price = None
+        self.old_price = None
+        self.price_changed = None
--- src/main.py
+++ src/main.py
@@ -1,16 +1,26 @@
 import telebot  # telegram-bot API
+from src.favorites_unit import FavoritesUnit
 
 # read key from txt file
+
 f = open('/home/snfn/PycharmProjects/keys/telegram_key')
 key_value = str(f.read()).rstrip()
 bot = telebot.TeleBot(key_value)
+f.close()
 
 # keyboard config
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
 keyboard1.row('Favorites', 'Add to favorites', 'Help')
 # keyboard1.row('Favorites', 'Add to favorites', 'Delete from favorites', 'Settings', 'Help')
 
-favorites_list = ['URL1', 'URL2']
+favorites_list = []
+favorites_list.extend(['URL1', 'URL2', 'URL3', 'URL4', 'URL5'])
+fav_len = len(favorites_list)
+favorite_data = ""
+
+for i in range(fav_len):
+    temp_string = '{0}. {1}\n'.format(i, favorites_list[i])
+    favorite_data += temp_string
 
 
 @bot.message_handler(commands=['start'])
@@ -21,7 +31,7 @@ def start_message(message):
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text == "Favorites":
-        bot.send_message(message.chat.id, favorites_list[0] + '\n' + favorites_list[1])
+        bot.send_message(message.chat.id, favorite_data)
     elif message.text == 'Add to favorites':
         bot.send_message(message.chat.id, 'Bye general Kenobi')
     elif message.text == 'Help':
